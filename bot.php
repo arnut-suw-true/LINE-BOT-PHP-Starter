880d07b94a1dbaefa8e45f7fbd810c76606fead9
arnut-suw-true <35957987+arnut-suw-true@users.noreply.github.com>
2018-01-30 18:31:49 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'แกนั่นแหละ'.$text.
+				'text' => 'แกนั่นแหละ'.$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
