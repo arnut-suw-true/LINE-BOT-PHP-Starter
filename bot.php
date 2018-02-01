6b0de4327960358094d5350ca21521d53e0509b1
arnut-suw-true <35957987+arnut-suw-true@users.noreply.github.com>
2018-02-01 10:24:38 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -16,11 +16,18 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+			if(strpos($text, 'TestBOT') !== false){
+			$messages = [
+				'type' => 'text',
+				'text' => 'แกนั่นแหละ' . str_replace("TestBOT","",$text)
+			];
+			}else{
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => 'แกนั่นแหละ' . $text
 			];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
